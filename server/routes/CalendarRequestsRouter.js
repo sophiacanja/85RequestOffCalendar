987aefeadae68bb28637cd0a33fb9c4b398f9d66
sophiacanja <sophia.canja1@gmail.com>
2023-06-06 14:28:32 -0700

    separated edge cases for deleteRequest and also separated helper function into 2 diff functions

--- server/routes/CalendarRequestsRouter.js
+++ server/routes/CalendarRequestsRouter.js
@@ -14,17 +14,28 @@ calendarRequestsRouter.post("/createRequest", async (req, res) => {
         const firstName = req.body.firstName
         const lastName = req.body.lastName
         const empID = req.body.employeeID
+        let message
         
-        //edge case: empty array, wrong format, already there
-        if(dates.length === 0 || await IncorrectDateOrAlreadyExists(dates)){
+        //handling edge cases: empty array, wrong format, already there
+        if(dates.length === 0){ 
+            message = "No date was entered"
+        }
+        if( await IncorrectDate(dates)){        //throws error if this is date entered 123-1-2023
+            message = "Please check the format of your date request"
+        }
+        if( await DateAlreadyExists(dates)){
+            message = "Date already requested"
+        }
+        if(dates.length === 0 || await IncorrectDate(dates) || await DateAlreadyExists(dates)){
             return res.status(400).send({
                 success : false,
-                message : "Improper date(s) were requested"
+                message : message
             })
         }
+        
 
-        // let createdDates = [];
-        //create a a new calendar request for each date 
+
+        //create and save a new calendar request for each date 
         for(const date of dates){   
             const calendarRequest = new CalendarRequestsModel({
                 employeeID: empID, 
@@ -33,15 +44,13 @@ calendarRequestsRouter.post("/createRequest", async (req, res) => {
                 date: date
             })
             await calendarRequest.save(); 
-
-            // createdDates.push(calendarRequest);
         }
 
         //sends success message
         return res.status(200).send({
             success: true,
             message: "Requests successfully created"
-            // data: createdDates
+           
         });
 
     } catch (err) {
@@ -68,7 +77,7 @@ calendarRequestsRouter.delete("/deleteRequest", async (req, res) => {
             })
         }
 
-        //request was successfully deleted
+        //sends success message
         return res.status(200).send({
             success: true,
             message: "Request successfully deleted"
@@ -84,26 +93,42 @@ calendarRequestsRouter.delete("/deleteRequest", async (req, res) => {
 
 
 calendarRequestsRouter.get("/getAllRequestsForDate", async (req, res) => {  //invalid date, no date, multiple requests  
-    const date = req.query.date
+    const date = new Date(req.query.date)
+
+    //check if invalid date or 
 
 
 });
 
 
 
-//helper function for createRequest
-const IncorrectDateOrAlreadyExists = async (dates) => {
+//helper function for createRequest that returns true if dates[] passed in has an incorrect format 
+const IncorrectDate = async (dates) => {
     // regex format mm-dd-yyyy
     const dateRegex = /^(0[1-9]|1[0-2])-(0[1-9]|1\d|2\d|3[01])-(\d{4})$/;
     for(const date of dates){
-        const dateExistsForUser = await CalendarRequestsModel.findOne({ date: date })
 
-        if(!dateRegex.test(date) || dateExistsForUser){
-            return true;
+        //checks if correct format or date exists in database
+        if(!dateRegex.test(date)){
+            return true
         }
     }
-    return false;
+    return false
 };
+
+//helper function for createRequest that returns true if dates[] passed in have already been requested
+const DateAlreadyExists = async (dates) => {
+    for (const date of dates) {
+        //checks if date is already in the database
+        const dateExistsForUser = await CalendarRequestsModel.findOne({ date: date })
+        if(dateExistsForUser){
+            return true
+        }
+    }
+    return false
+}
+
+
 
 
 
