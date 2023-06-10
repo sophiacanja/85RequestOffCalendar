6f767a295f24b71cde2e5255c07a25c4ba59ca19
sophiacanja <sophia.canja1@gmail.com>
2023-06-09 22:16:46 -0700

    declared variables needed

--- src/components/UpdateAccount.js
+++ src/components/UpdateAccount.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Form, Button, InputGroup, FormControl, Modal } from "react-bootstrap";
+import Axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 const UpdateAccount = () => {
-  // Component logic and state
+  const [email, setEmail] = useState("");
+  const [employeeID, setEmployeeID] = useState("");
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const [showPasswordChangeModal, setShowChangePasswordModal] = useState(false);
+  const [showEnterNewPasswordModal, setShowEnterNewPasswordModal] = useState(false);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [showDeletionModal, setShowDeletionModal] = useState(false);
+
+  const [originalEmail, setOriginalEmail] = useState("");
+  const [originalPassword, setOriginalPassword] = useState("");
+  const [originalFirstName, setOriginalFirstName] = useState("");
+  const [originalLastName, setOriginalLastName] = useState("");
+
+  const navigate = useNavigate();
+
 
   return (
     <div>
