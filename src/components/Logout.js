f12b36e78174b3d2652d4ede10300dfd9e409437
sophiacanja <sophia.canja1@gmail.com>
2023-07-01 22:11:55 -0700

    finished logout functionality

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,35 +1,15 @@
-import { React, useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import Axios from 'axios';
-import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import {CheckIfLoggedIn} from "./PrivateRouterUser.js";
-// import "./Logout.css";
 
 const Logout = () => {
-  const [logoutSuccess, setLogoutSuccess] = useState(null);
 
-  //updates the isLoggedIn field 
-  const logout = async () => {
-    try {
-      if(CheckIfLoggedIn === true){
-        console.log("User is logged in, logging out now")
-        const status = localStorage.getItem("userIsLoggedIn")
-        localStorage.setItem(false, status)
-        console.log("User is logged out, status of userIsLoggedIn: ")
-        console.log(localStorage.userIsLoggedIn)
-
-      }
-      else {
-        console.log("User was never logged in")}
-    } catch (err) {
-
-    }
+  const checkoutUser = async () => {
+    localStorage.token = null; 
+    window.location.reload();
   }
 
   return (
     <div className="Logout verification"> Are you sure you want to log out?
-      <Button block="true" onClick={() => { logout() }}> Logout </Button>
+      <Button block="true" onClick={checkoutUser}> Logout </Button>
     </div>
   );
 };
