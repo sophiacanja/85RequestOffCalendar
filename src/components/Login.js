0f506bc31bd3b3e6224b041d56dcb7857730644e
sophiacanja <sophia.canja1@gmail.com>
2023-07-21 09:51:24 -0700

    messing around w login layout

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,11 +3,9 @@ import { useLocation } from 'react-router-dom';
 import { Form } from "react-bootstrap";
 import Axios from 'axios';
 import Button from "react-bootstrap/Button";
-import catLogo from '../assets/gifs/catLoading.gif';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import cakeImage from '../assets/photos/85Bakery_redVelvet.jfif'
 import Card from 'react-bootstrap/Card';
 
 import "./Login.css";
@@ -53,7 +51,6 @@ const Login = () => {
   }
 
 
-
   useEffect(() => {
     const state = location.state?.message;
     if (state !== null && state !== undefined) {
@@ -61,10 +58,7 @@ const Login = () => {
     } else {
       setAuthenticationStatus("Please enter your login credentials");
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-
 
 
 
@@ -73,71 +67,67 @@ const Login = () => {
   }
 
   return (
-    <div className = "loginPage"> 
     <Container className= "LoginContainer" fluid="lg"> 
   
     
-    <Container className="LoginFormContainer" fluid="sm">
+    <Container className="login form justify-content-center align-items-center align-items-center 100-w 100vh bg-white" id="LoginFormContainer" fluid="sm">
       <Row> 
-      <div className="text-center">  
-      <h1 className="Title"> 85c Login  </h1>
-      </div>
+        <div className="text-center">  
+        <h1 className="Title"> 85c Login  </h1>
+        </div>
       </Row>
      
-      <Form className= "LoginForm" >
-      <Row> 
-       
-        <Form.Group controlId="employeeID">
-          <Form.Control
-            autoFocus
-            placeholder="Employee ID"
-            type="text"
-            value={employeeID}
-            onChange={(e) => setEmployeeID(e.target.value)}
-          />
-        </Form.Group>
-      </Row> 
+      <Form className= "40-w p-5 rounded"  id="LoginForm ">
+        <Row> 
+          <Form.Group controlId="employeeID">
+            <Form.Control
+              autoFocus
+              placeholder="Employee ID"
+              type="text"
+              value={employeeID}
+              onChange={(e) => setEmployeeID(e.target.value)}
+            />
+          </Form.Group>
+        </Row> 
         
         <Row> 
-        <Form.Group  controlId="password">
-          <Form.Control
-            type="text"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
+          <Form.Group  controlId="password">
+            <Form.Control
+              type="text"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
         </Row>
+        
        
         <div className = "text-center">
        
           <Button className="login-button" onClick={login} disabled={!validateForm()} >
             Login
           </Button>
-          {authenticationStatus === true && <div> Success! Redirecting to Home Page</div>}
+          {authenticationStatus === true && <div className="SuccessMessage"> Success! Redirecting to Home Page</div>}
           {authenticationStatus === false && <div> Incorrect credentials, please login again</div>}
-        <Row> 
-        <div className="forgot-password">
-          {/* TODO put in correct /path for href */}
-          <a className="forgot-password" href="/login" onClick={resetPassword}>Forgot Password?</a>
-        </div>
-        </Row>
+          <Row> 
+            <div className="forgot-password">
+            {/* TODO put in correct /path for href */}
+              <a className="forgot-password" href="/login" onClick={resetPassword}>Forgot Password?</a>
+            </div>
+          </Row>
         </div>
       </Form>
 
       <Row> 
-      <div className="text-center">
-      <div className="StatusMessage">Status: {authenticationStatus}</div>
-      </div>
+        <div className="text-center">
+        <div className="StatusMessage">Status: {authenticationStatus}</div>
+        </div>
       </Row>
 
     </Container>
    
     </Container>
     
-    
-    
-    </div>
 
   )
 };
