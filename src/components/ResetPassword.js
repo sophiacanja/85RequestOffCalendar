520755a04766f2f5894a08c5f74329bd0238e1b1
sophiacanja <sophia.canja1@gmail.com>
2023-07-28 17:32:18 -0700

    beautified page

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Axios from 'axios';
+import { Form } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './ResetPassword.css';
+import resetPasswordBackground from "../assets/photos/forgotPasswordBackground.jpg"
+
 
 const ResetPassword = () => {
   // "http://localhost:3000/resetPassword?instance=123"
@@ -90,59 +98,77 @@ const ResetPassword = () => {
 
 
   return (
-    <div>
+    <div style= {{ backgroundImage: `url(${resetPasswordBackground})`, backgroundRepeat: "no-repeat", 
+      backgroundSize: "cover", height: '100vh', margin: 0, padding: 0}}>.
+      
+    <Container className="rounded" id="ResetPasswordContainer">
+    <Container className="justify-content-center align-items-center align-items-center w-75 bg-white rounded" id="LoginFormContainer" fluid="sm">
       {(() => {
         if (stillVerifyingToken === true) {
           return (
-            <>
+            <Row> 
               <h2>Loading...</h2>
-            </>
+            </Row>
           )
         } else if (authStatus === "error") {
           return (
-            <>
+            <Row> 
               <h2>Error</h2>
-            </>
+            </Row>
           )
         } else if (authStatus === "expired") {
           return (
-            <>
+            <Row> 
               <h2>Expired</h2>
-            </>
+            </Row>
           )
         } else if (authStatus === "invalid token") {
           return (
-            <>
+            <Row>
               <h2>Invalid token</h2>
-            </>
+            </Row>
           )
         } else if (authStatus === "success") {
           return (
-            <>
+            <Row>
               <h2>Please enter your new password</h2>
-              <form
-                onSubmit={handleSubmit}
-                name="password">
-                <div>
-                  <label>Enter New Password:</label>
-                  <input
-                    type="password"
-                    onChange={UpdateUserInfoAndPassword}
-                    required
-                  />
-                  <label>Confirm Password:</label>
-                  <input
-                    type="password"
-                    onChange={(e) => setSecondPassword(e.target.value)}
-                    required
-                  />
-                </div>
-                <button type="submit">Set New Password</button>
-              </form>
-            </>
+              <p> Passwords must be a minimum of 8 characters and must contain at least one 
+                numerical digit (0-9), one upper case letter, and one lower case letter </p>
+              <Form onSubmit={handleSubmit}>
+                  <Form.Group as={Row} className="mb-3"> 
+                      <Form.Label column sm="5"> Enter New Password:</Form.Label>
+                      <Col sm="7">
+                      <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        onChange={UpdateUserInfoAndPassword}
+                        required
+                      />
+                      </Col>
+                      </Form.Group>
+            
+                  <Form.Group as={Row} className="mb-3"> 
+                    <Form.Label column sm="5">Confirm Password:</Form.Label>
+                    <Col sm="7">
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      onChange={(e) => setSecondPassword(e.target.value)}
+                      required
+                    />
+                    </Col>
+                  </Form.Group>
+
+                <Row> 
+                  <Button type="submit">Set New Password</Button>
+                </Row>
+                </Form>
+            </Row>
           )
         }
       })() /* IIFE immediately invoked function expression */}
+    </Container>
+    </Container>
     </div>
   )
 };
